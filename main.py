13d9f8c176a03303a887f491fbd5fe393cd2a933
ninjasgithub <60543166+ninjasgithub@users.noreply.github.com>
2020-05-14 21:31:15 +0100

    Added file

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests, time
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+KEY = "AIzaSyAvclkDd9XDOG3AKOk-xhMegvhwyq0_1gU"
+SECRET = 'secret.json'
+scopes = ["https://www.googleapis.com/auth/youtube"]
+
+flow = InstalledAppFlow.from_client_secrets_file(SECRET, scopes)
+creds = flow.run_console()
+youtube = build('youtube', 'v3', credentials=creds)
+
+def getViewCount(id, API_KEY):
+    r = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=statistics&id={id}&key={API_KEY}')
+    res = r.json()['items'][0]['statistics']['likeCount']
+    print(r.json())
+    return res
+
+def updateViewCount(v):
+    data = {
+          'id': '2ZKmKoC_fgk',
+          'snippet': {
+            'title': f'This video has {v} likes',
+            'categoryId': '22',
+          }
+        }
+    req = youtube.videos().update(part="snippet", body=data)
+    res = req.execute()
+    print(res)
+    
+while True:
+    views = getViewCount("2ZKmKoC_fgk", KEY)
+    print(views)
+    updateViewCount(views)
+    time.sleep(30)
